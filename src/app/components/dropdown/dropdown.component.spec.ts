760414e01caf8121aeb5498881610b9ee409c5b2
David Nin <dninmur@gmail.com>
2022-12-12 18:48:16 +0100

    dropdown test

--- src/app/components/dropdown/dropdown.component.spec.ts
+++ src/app/components/dropdown/dropdown.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 import { DropdownComponent } from './dropdown.component';
+
+import Dropdown from 'src/app/models/dropDown';
+
 
 describe('DropdownComponent', () => {
   let component: DropdownComponent;
@@ -8,16 +13,64 @@ describe('DropdownComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DropdownComponent ]
+      declarations: [DropdownComponent],
+      imports: [FontAwesomeModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(DropdownComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create correct the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the default values correctly', () => {
+    expect(component.showDropdown).toBeFalse();
+    expect(component.SelectedItem).toBe('');
+    expect(component.iconArrowUp).toBe(faArrowUp);
+    expect(component.iconArrowDown).toBe(faArrowDown);
+  })
+
+  it('should change showDropdown when call toggle', () => {
+    expect(component.showDropdown).toBeFalse();
+    component.toggle()
+    expect(component.showDropdown).toBeTrue();
+  })
+
+  it('should change showDropdown when call toggle twice', () => {
+    expect(component.showDropdown).toBeFalse();
+    component.toggle()
+    expect(component.showDropdown).toBeTrue();
+    component.toggle()
+    expect(component.showDropdown).toBeFalse();
+  })
+
+  it('when call select, should call the output function and set new selectedItem', () => {
+    spyOn(component.onChangeValueDropdown, 'emit');
+    let newOptionSelected: Dropdown = { label: 'Name', value: 'name' };
+    let expectSelectItemData: string = 'Name';
+    let expectEmitData: string = 'name';
+    component.showDropdown = true;
+
+    expect(component.SelectedItem).toBe('');
+
+    component.select(newOptionSelected);
+
+    expect(component.SelectedItem).toBe(expectSelectItemData);
+    expect(component.onChangeValueDropdown.emit).toHaveBeenCalledWith(expectEmitData)
+    expect(component.showDropdown).toBeFalse();
+  })
+
+  it('when i click toggle', fakeAsync(() => {
+    spyOn(component, 'toggle');
+
+    let button = fixture.debugElement.nativeElement.querySelector('#toogleDiv');
+    button.click();
+
+    tick();
+    expect(component.toggle).toHaveBeenCalled();
+  }))
 });

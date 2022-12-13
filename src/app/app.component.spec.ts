52258e1d4325989d36a8338f812dd462c6926253
David Nin <dninmur@gmail.com>
2022-12-13 19:15:18 +0100

    ready to test the main app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { InternalSearchComponent } from './components/internal-search/internal-search.component';
+import { PopupErrorComponent } from './components/popup-error/popup-error.component';
+import { ProductViewComponent } from './views/product-view/product-view.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        FontAwesomeModule,
+        NgxSliderModule,
+        FormsModule,
+        HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        DropdownComponent,
+        InternalSearchComponent,
+        PopupErrorComponent,
+        ProductViewComponent,
+        ModalComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'wallaTechTest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('wallaTechTest');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wallaTechTest app is running!');
+    expect(component.title).toEqual('wallaTechTest');
   });
 });
